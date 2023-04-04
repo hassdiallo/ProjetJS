3d439425e6b82e32fcade1d9ca61e349ce293bbb
Hassimiou Diallo <hsmdiallo@gmail.com>
2023-04-04 03:39:32 +0200

    Projet js estiam prémière version

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
 const express = require("express")
 const app = express()
+const cors = require('cors')
 const PORT = 5000
 const connectDB = require("./config/db")
+const corsOption = {
+    origin:"http://localhost:3000",
+    optionsSuccessStatus:200
+}
 
 connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
+app.use(cors(corsOption))
 
 app.use('/students',require("./routes/student.route"))
 
